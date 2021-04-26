7ae7d019e5595c4cbc88e4475fbfad774283356d
BigMove <dorbor@bigmove.io>
2021-04-25 19:54:29 -0700

    validation initiated

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt =     require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// load input validation 
+const validateRegisterInput =  require('../../validation/register');
+
 // load user model
 const User = require('../../models/User');
 const { route } = require('./profile');
@@ -20,6 +23,13 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 
 
 router.post('/register', (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
     User.findOne({email: req.body.email })
     .then(user => {
         if(user){
